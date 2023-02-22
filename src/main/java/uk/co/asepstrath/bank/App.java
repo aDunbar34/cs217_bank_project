b1813e1131eb694795110b88b12f7f48f8a3cb66
Lauren <lauren.knox.2021@uni.strath.ac.uk>
2023-02-22 12:51:53 +0000

    final push with integration test

--- src/main/java/uk/co/asepstrath/bank/App.java
+++ src/main/java/uk/co/asepstrath/bank/App.java
@@ -37,6 +37,7 @@ public class App extends Jooby {
         Logger log = getLog();
 
         mvc(new ExampleController(ds,log));
+        mvc(new Controller());
 
         /*
         Finally we register our application lifecycle methods
--- src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
+++ src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
@@ -1,13 +1,31 @@
 package uk.co.asepstrath.bank;
 
+import uk.co.asepstrath.bank.App;
 import io.jooby.JoobyTest;
+import io.jooby.StatusCode;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @JoobyTest(App.class)
 public class IntegrationTest {
-    /*
-    Integration tests should be here
-    Example can be found in example/IntegrationTest.java
-     */
+
+    static OkHttpClient client = new OkHttpClient();
+
+    @Test
+    public void shouldDisplay(int serverPort) throws IOException {
+        Request req = new Request.Builder()
+                .url("http://localhost:" + serverPort+"/accounts")
+                .build();
+
+        try (Response rsp = client.newCall(req).execute()) {
+            assertEquals("[Account Name: Rachel Balance:Â£ 50.0, Account Name: Monica Balance:Â£ 1000.0, Account Name: Phoebe Balance:Â£ 76.0, Account Name: Joey Balance:Â£ 23.9, Account Name: Chandler Balance:Â£ 3.0, Account Name: Ross Balance:Â£ 54.32]", rsp.body().string());
+            assertEquals(StatusCode.OK.value(), rsp.code());
+        }
+    }
 }
