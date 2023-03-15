eb6b3e2aeafe90d150f09267bd7d97e06a8aa3c5
Lauren <lauren.knox.2021@uni.strath.ac.uk>
2023-03-15 12:21:00 +0000

    attempt at unit tests

--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -1,20 +1,95 @@
-package uk.co.asepstrath.bank;
+//package uk.co.asepstrath.bank;
+//
+//import io.jooby.ModelAndView;
+//import org.junit.jupiter.api.Test;
+//
+//import javax.sql.DataSource;
+//import java.math.BigDecimal;
+//import java.sql.Connection;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//import java.util.ArrayList;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.Mockito.when;
+//
+//public class UnitTest {
+//
+//        @Test
+//        public void testGetHome() {
+//            Controller controller = new Controller(null, null);
+//            ModelAndView modelAndView = controller.getHome();
+//            assertEquals("home.hbs", modelAndView.getViewName());
+//        }
+//
+//        @Test
+//        public void testGetAbout() {
+//            Controller controller = new Controller(null, null);
+//            ModelAndView modelAndView = controller.getAbout();
+//            assertEquals("about.hbs", modelAndView.getViewName());
+//        }
+//
+//    public void testParseJSON() {
+//        Controller controller = new Controller(mock(DataSource.class), mock(Logger.class));
+//        String json = "[{\"id\": \"1\",\"name\": \"John\",\"balance\": 1000.0,\"currency\": \"USD\",\"accountType\": \"Checking\"}]";
+//        ArrayList<Account> accounts = controller.parseJSON(json);
+//        assertEquals(1, accounts.size());
+//        assertEquals("1", accounts.get(0).getId());
+//        assertEquals("John", accounts.get(0).getName());
+//        assertEquals(new BigDecimal("1000.0"), accounts.get(0).getBalance());
+//        assertEquals("USD", accounts.get(0).getCurrency());
+//        assertEquals("Checking", accounts.get(0).getAccountType());
+//    }
+//
+//    @Test
+//    public void testRetrieveData() throws SQLException {
+//        DataSource dataSource = mock(DataSource.class);
+//        Connection connection = mock(Connection.class);
+//        Statement statement = mock(Statement.class);
+//        ResultSet resultSet = mock(ResultSet.class);
+//        when(dataSource.getConnection()).thenReturn(connection);
+//        when(connection.createStatement()).thenReturn(statement);
+//        when(statement.executeQuery(anyString())).thenReturn(resultSet);
+//        when(resultSet.next()).thenReturn(true, false);
+//        when(resultSet.getString("id")).thenReturn("1");
+//        when(resultSet.getString("name")).thenReturn("John");
+//        when(resultSet.getBigDecimal("balance")).thenReturn(new BigDecimal("1000.0"));
+//        when(resultSet.getString("currency")).thenReturn("USD");
+//        when(resultSet.getString("accountType")).thenReturn("Checking");
+//        Controller controller = new Controller(dataSource, mock(Logger.class));
+//        ArrayList<Account> accounts = controller.retreiveData();
+//        assertEquals(1, accounts.size());
+//        assertEquals("1", accounts.get(0).getId());
+//        assertEquals("John", accounts.get(0).getName());
+//        assertEquals(new BigDecimal("1000.0"), accounts.get(0).getBalance());
+//        assertEquals("USD", accounts.get(0).getCurrency());
+//        assertEquals("Checking", accounts.get(0).getAccountType());
+//    }
+//
+//    @Test
+//    public void testParseJSONTransaction() {
+//        Controller controller = new Controller(mock(DataSource.class), mock(Logger.class));
+//        String json = "[{\"withdrawAccount\":\"1\",\"depositAccount\":\"2\",\"timestamp\":\"2022-02-22T22:22:22.000Z\",\"id\":\"1\",\"amount\":1000.0,\"currency\":\"USD\"}]";
+//        ArrayList<Transaction> transactions = controller.parseJSONTransaction(json);
+//        assertEquals(1, transactions.size());
+//        assertEquals("1", transactions.get(0).getTransactionID());
+//        assertEquals("1", transactions.get(0).getWithdrawAcc());
+//        assertEquals("2", transactions.get(0).getDepositAcc());
+//        assertEquals(new BigDecimal("1000.0"), transactions.get(0).getAmount());
+//        assertEquals("USD", transactions.get(0).getCurrency());
+//        assertEquals("2022-02-22T22:22:22.000Z", transactions.get(0).getTimestamp());
+//    }
 
-import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
-import kong.unirest.Unirest;
-import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class UnitTest {
-    /*
-    Unit tests should be here
-    Example can be found in example/UnitTest.java
-     */
 
-}
+
+
+
+
+
+
+
+
